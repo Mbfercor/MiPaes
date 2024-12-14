af5a30496cf6b3eb6db8fc85298d5e280670bf7f
mbfercor <mbfercor@gmail.com>
2024-12-13 21:31:58 -0300

    13/12/2024. Se sube splashscreen básica.

--- mi_paes/lib/main.dart
+++ mi_paes/lib/main.dart
@@ -1,4 +1,6 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'package:mi_paes/screens/splashscreen.dart'; // Importa el archivo splashscreen.dart
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      themeMode: ThemeMode.system, // Cambia según la configuración del sistema
+      home: const SplashScreen(), // La primera pantalla será el SplashScreen
+    );
+  }
+}
+
+// Aquí tienes la pantalla que será el home después del splash
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold (
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //Centra la columna verticalmente
+          children: [
+            Text('Soy el Home de la app'),
+            Text('Soy el Home de la app'),
+            Text('Soy el Home de la app'),
+            Text('Soy el Home de la app'),
+            Text('Soy el Home de la app'),
+          ],
         ),
       ),
-    );
+    ); // 
   }
 }
